0102c1204061bea56c9a98a0bdc95754c4320657
AndyB-WHG <andrew.bond2@gmail.com>
2021-11-17 18:48:17 +0000

    Setup Allauth.

--- e_volve_retail/settings.py
+++ e_volve_retail/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    # below used to generate correct urls when connecting to social media
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',			            # allauth app itself
+    'allauth.account',		        # login, logout, register, password resets app
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +74,26 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1   # used to generate correct urls when connecting to social media
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'e_volve_retail.wsgi.application'
 
