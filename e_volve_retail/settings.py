640f7c6e91dbe170ddf401193478ac3a46995807
AndyB-WHG <andrew.bond2@gmail.com>
2021-11-17 19:36:58 +0000

    Amended base template. Added 'home' app, view and url.

--- e_volve_retail/settings.py
+++ e_volve_retail/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth',			            # allauth app itself
     'allauth.account',		        # login, logout, register, password resets app
     'allauth.socialaccount',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,10 @@ ROOT_URLCONF = 'e_volve_retail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
