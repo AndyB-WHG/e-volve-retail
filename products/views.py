97992898f88cf7790725231018fbd4c505824171
AndyB-WHG <andrew.bond2@gmail.com>
2021-12-09 12:13:29 +0000

    Basic 'Search' function enabled.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+""" Views to display 'All Products' and individual Product Details """
+
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+# Note: 'Q' Enables searches where the query
+# can be in either one list 'or' another
 from .models import Product
 
 
@@ -6,9 +12,25 @@ def all_products(request):
     """ A view to return all the products, including sorting and searching """
 
     products = Product.objects.all()
+    query = None  # Resets the 'query' value in case it contain a previous value.
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               "You didn't enter any search criteria.")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                        description__icontains=query)
+            # Note: 'the 'i' in icontains means 'case insensitive'.
+
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
