b7d5d284cb63c0a43b97de2f05b6f9436d74f3cc
AndyB-WHG <andrew.bond2@gmail.com>
2021-12-19 22:13:01 +0000

    Toasts completed - but has error rendering to page.

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -12,7 +12,7 @@ def shopping_bag(request):
 def add_to_shopping_bag(request, item_id):
     """ Add a quantity of a given item to the Shopping Bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     size = None
     if 'size' in request.POST:
         size = request.POST.get('size')
@@ -33,20 +33,25 @@ def add_to_shopping_bag(request, item_id):
             if size in shopping_bag_session[item_id]['items_by_size'].keys():
                 shopping_bag_session[item_id]
                 ['items_by_size'][size] += quantity
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {shopping_bag_session[item_id]["items_by_size"]}')
             else:
                 shopping_bag_session[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
         else:
             shopping_bag_session[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
     else:
         if item_id in list(shopping_bag_session.keys()):
             shopping_bag_session[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {shopping_bag_session[item_id]}')
         else:
             shopping_bag_session[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
             
         # Checks if the session already includes the item.
         # If it does, it add the quantity to the existing quanity.
         # If it doesn't it add the item_id and it's related quantity.
-    messages.success(request, f'Added {product.name} to your bag')
+    
 
     request.session['shopping_bag_session'] = shopping_bag_session
     # Over-writes the original shopping bag session with the updated version.
@@ -58,7 +63,7 @@ def adjust_shopping_bag(request, item_id):
     """ Allows user to change item quantity from within the shopping bag """
 
     print("starting the 'Adjust Shopping Bag' function")
-    # print("Request : ", request)
+    product = get_object_or_404(Product, pk=item_id)
     size = None
     if 'product_size' in request.POST:
         size = request.POST.get('product_size')
@@ -75,18 +80,22 @@ def adjust_shopping_bag(request, item_id):
     if size:
         if quantity > 0:
             shopping_bag_session[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {shopping_bag_session[item_id]["items_by_size"]}')
         else:
             del shopping_bag_session[item_id]['items_by_size'][size]
             if not shopping_bag_session[item_id]['items_by_size']:
                 shopping_bag_session.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
 
     else:
         print("There was no size. Replacing previous qty with new quantity.")
         if quantity > 0:
             shopping_bag_session[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {shopping_bag_session[item_id]}')
             print("Updated shopping_bag_session : ", shopping_bag_session)
         else:
             shopping_bag_session.pop[item_id]
+            messages.success(request, f'Removed {product.name} from your shopping bag')
 
     request.session['shopping_bag_session'] = shopping_bag_session
     # Over-writes the original session cookie with the updated version.
@@ -97,19 +106,20 @@ def adjust_shopping_bag(request, item_id):
 def delete_from_shopping_bag(request, item_id):
     """ Remove an item from the shopping bag """
 
-    print("starting the 'Delete From Shopping Bag' function")
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST.get('product_size')
-
-    shopping_bag_session = request.session.get('shopping_bag_session', {})
-    # Note: above gets the session called shopping_bag, but if such
-    # a session doesn't exist, it creates an empty dictionary instead.
-
-    print("'Delete item' shopping bag session: ", shopping_bag_session)
-    print("Item ID being updated : ", item_id)
-
     try:
+        print("starting the 'Delete From Shopping Bag' function")
+        product = get_object_or_404(Product, pk=item_id)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST.get('product_size')
+
+        shopping_bag_session = request.session.get('shopping_bag_session', {})
+        # Note: above gets the session called shopping_bag, but if such
+        # a session doesn't exist, it creates an empty dictionary instead.
+
+        print("'Delete item' shopping bag session: ", shopping_bag_session)
+        print("Item ID being updated : ", item_id)
+    
         if size:
             # Delete the item with the specific size
             del shopping_bag_session[item_id]['items_by_size'][size]
@@ -117,9 +127,11 @@ def delete_from_shopping_bag(request, item_id):
             # delete the item entirely.
             if not shopping_bag_session[item_id]['items_by_size']:
                 shopping_bag_session.pop(item_id)
+                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
 
         else:
             shopping_bag_session.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your shopping bag')
 
         request.session['shopping_bag_session'] = shopping_bag_session
         # Over-writes the original session cookie with the updated version.
@@ -127,4 +139,5 @@ def delete_from_shopping_bag(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=200)
