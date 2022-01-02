b09646f240a82161f54f074da08cfb867bce6d2f
AndyB-WHG <andrew.bond2@gmail.com>
2022-01-02 11:53:00 +0000

    Reviews template created.

--- user_reviews/views.py
+++ user_reviews/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . models import User_review
 
-# Create your views here.
+
+def get_all_reviews(request):
+    # create the 'query object' from which we can interrogate the data
+    reviews = User_review.objects.all()
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'user_reviews/all_reviews.html', context)
