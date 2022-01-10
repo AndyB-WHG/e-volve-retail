bc35c544dcd5ce3ec3c6fe53c2208e010748175b
AndyB-WHG <andrew.bond2@gmail.com>
2022-01-10 09:32:48 +0000

    Forms added for Newsletter and Subscriber pages.

---
+++ newsletters/forms.py
@@ -0,0 +1,26 @@
+""" Generate 'Create Newsletter' form within 'new_newsletter page """
+
+from django import forms
+from .models import Newsletters, Subscribers
+
+
+class NewsletterForm(forms.ModelForm):
+    """ Create Newsletter Form """
+    class Meta:
+        """ Choose fields req'd - fields taken from 'Newsletters' model """
+        model = Newsletters
+        fields = ('title', 'body',)
+
+    def __init__(self, *args, **kwargs):
+        self.fields['title'].widget.attrs['autofocus'] = True
+
+
+class SubscriberForm(forms.ModelForm):
+    """ Create form for users to Subscribe to the Newsletters """
+    class Meta:
+        """ Choose fields req'd - fields taken from 'Subscribers' model """
+        model = Subscribers
+        fields = ('firstName', 'email',)
+
+    def __init__(self, *args, **kwargs):
+        self.fields['firstName'].widget.attrs['autofocus'] = True
--- newsletters/views.py
+++ newsletters/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from . forms import NewsletterForm, SubscriberForm
 
 
 def new_newsletter(request):
+    form = NewsletterForm()
+    context = {
+        'form': form,
+    }
     return render(request, 'newsletters/new_newsletter.html')
+
+
+def new_subscriber(request):
+    form = SubscriberForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'newsletters/new_subscriber.html', context)
